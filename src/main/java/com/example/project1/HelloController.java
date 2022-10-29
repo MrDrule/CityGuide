f198b42baa9c7f74569ffc979001385db3d38974
IntelliJeo24 <102180891+IntelliJeo24@users.noreply.github.com>
2022-10-29 18:30:04 +0300

    v1

---
+++ src/main/java/com/example/project1/HelloController.java
@@ -0,0 +1,56 @@
+package com.example.project1;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToFav(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Favourites.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToProf(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToRec(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Recommended.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToNearby(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Nearby.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
